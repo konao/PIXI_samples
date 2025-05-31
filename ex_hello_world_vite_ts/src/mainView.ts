f390c64f6d4fa969ae627aaa8c0e7d0d22e63c3d
Takuya Sekikawa <konao235@gmail.com>
2025-05-31 16:55:43 +0900

    微修正

--- ex_hello_world_vite_ts/src/mainView.ts
+++ ex_hello_world_vite_ts/src/mainView.ts
@@ -7,33 +7,30 @@
 import * as PIXI from 'pixi.js';
 // import $ from 'jquery';
 
-(async () =>
-{
+(async () => {
     try {
-    // Create a new application
-    const app = new PIXI.Application();
+        // Create a new application
+        const app = new PIXI.Application();
 
-    await app.init({
-        // width: 800,
-        // height: 600,
-        background: '#1099bb', 
-        resizeTo: window, 
-        antialias: true,
-    })
+        await app.init({
+            background: '#1099bb',
+            resizeTo: window,
+            antialias: true,
+        })
 
-    document.body.appendChild(app.canvas);
+        document.body.appendChild(app.canvas);
 
-    let g = new PIXI.Graphics();
+        let g = new PIXI.Graphics();
 
-    g.clear();
-    g.ellipse(200, 200, 150, 150);
-    g.fill("00ff00");
+        g.clear();
+        g.ellipse(200, 200, 150, 150);
+        g.fill("00ff00");
 
-    // v7までの書き方（v8で大きく変わったらしい）
-    // g.setStrokeStyle(10, 0x00ff00, 0.7);  // 太さ、色、アルファ(0=透明)
-    // g.ellipse(300, 300, 100, 100);  // 中心(cx, cy), 半径(rx, ry)
+        // v7までの書き方（v8で大きく変わったらしい）
+        // g.setStrokeStyle(10, 0x00ff00, 0.7);  // 太さ、色、アルファ(0=透明)
+        // g.ellipse(300, 300, 100, 100);  // 中心(cx, cy), 半径(rx, ry)
 
-    app.stage.addChild(g);
+        app.stage.addChild(g);
 
     } catch (error) {
         console.error('Error initializing PIXI application:', error);
