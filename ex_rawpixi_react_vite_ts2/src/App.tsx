6032eedcaaa04442f765f7d8e589e3ee2045fab4
Takuya Sekikawa <konao235@gmail.com>
2025-09-15 19:29:41 +0900

    画面のタイトルを変更

--- ex_rawpixi_react_vite_ts2/src/App.tsx
+++ ex_rawpixi_react_vite_ts2/src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import PixiCanvas from './PixiCanvas';
 
 function App() {
   return (
     <div>
-      <h1>React + PIXI.js v8</h1>
+      <h1>Solar System</h1>
       <PixiCanvas />
     </div>
   );
