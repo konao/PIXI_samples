27c6ab93672201b885c9679628fafe9059084749
Takuya Sekikawa <konao235@gmail.com>
2026-09-08 12:25:08 +0900

    feat: スペースバーで弾丸発射

--- ex_rawpixi_react_vite_ts5/src/PixiCanvas.tsx
+++ ex_rawpixi_react_vite_ts5/src/PixiCanvas.tsx
@@ -24,7 +24,8 @@ const keys: { [key: string]: boolean } = {
     ArrowUp: false,
     ArrowDown: false,
     ArrowLeft: false,
-    ArrowRight: false
+    ArrowRight: false,
+    Space: false
 };
 
 // 2. イベントリスナーを登録（キーが押されたか離されたかを記録）
@@ -33,11 +34,18 @@ window.addEventListener('keydown', (e) => {
         keys[e.key] = true;
         e.preventDefault(); // 画面がブラウザでスクロールするのを防ぐ
     }
+    if (e.code === 'Space') {
+        keys.Space = true;
+        e.preventDefault(); // スクロール防止
+    }    
 });
 
 window.addEventListener('keyup', (e) => {
     if (e.key in keys) {
         keys[e.key] = false;
+    }
+    if (e.code === 'Space') {
+        keys.Space = false;
     }
 });
 
@@ -51,8 +59,8 @@ export default function PixiCanvas() {
 
     // PIXI v8: 非同期初期化
     app.init({
-      width: 900,
-      height: 600,
+      width: 800,
+      height: 1024,
       background: '#002244',
       antialias: true,
     }).then(async () => {
@@ -80,19 +88,23 @@ export default function PixiCanvas() {
 
       // JSON内で定義した「スプライト名」で個別テクスチャを取り出せる
       const tex_player_b_m = wholeTexture.textures['Player/player_b_m.png'];
+      const tex_bullet = wholeTexture.textures["FX/vulcan_3.png"]
 
       // プレーヤーのスプライト（パーティクル）を生成
       const player = new PIXI.Particle(tex_player_b_m);
-      
+
       // 初期位置の設定
-      player.x = app.screen.width / 2;
-      player.y = app.screen.height * 2/3;
+      player.x = app.screen.width / 2 - 32;
+      player.y = app.screen.height * 4/5;
       // player.anchor.set(0.5); // 回転の中心を画像の中央にする
       
       particleContainer.addParticle(player);
 
       // 移動速度（1秒間に移動するピクセル数）
       const moveSpeed = 300;
+
+      // 弾丸
+      const bullets: PIXI.Particle[] = []
 
       app.ticker.add((ticker) => {
         // 前のフレームからの経過時間（秒に変換するためのデルタ値）
@@ -105,7 +117,43 @@ export default function PixiCanvas() {
         if (keys.ArrowUp)    player.y -= distance;
         if (keys.ArrowDown)  player.y += distance;
         if (keys.ArrowLeft)  player.x -= distance;
-        if (keys.ArrowRight) player.x += distance;      
+        if (keys.ArrowRight) player.x += distance;
+        if (keys.Space) {
+          keys.Space = false; // 1回押すごとに一発
+
+          // 弾丸のスプライトを生成
+          const bullet = new PIXI.Particle(tex_bullet)
+          bullet.x = player.x + 32;
+          bullet.y = player.y - 10;
+          bullets.push(bullet)
+          particleContainer.addParticle(bullet)
+        }
+
+        if (bullets.length > 0) {
+          const indsToRemove: number[] = [];
+
+          // 弾丸移動
+          for (let i=0; i<bullets.length; i++) {
+            const p = bullets[i]
+            p.y -= 10;
+            if (p.y < 0) {
+              // 画面を外れたものを削除対象に入れる
+              indsToRemove.push(i)
+            }
+          }
+
+          // 画面を外れた弾丸は消す
+          if (indsToRemove.length > 0) {
+            // 降順にソート
+            const sortedInds = indsToRemove.sort((a, b) => b-a)
+
+            for (const i of sortedInds) {
+              const bullet = bullets[sortedInds[i]]
+              particleContainer.removeParticle(bullet); // コンテナから消す
+              bullets.splice(i, 1); // i番目の要素を削除して配列の長さを縮める
+            }
+          }
+        }
       });
     });
 
